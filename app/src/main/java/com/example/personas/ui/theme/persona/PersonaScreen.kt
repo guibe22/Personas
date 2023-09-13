eab46a0012214e012a76bc57658ddf8570582be9
guibe22 <galvezwilber22@gmail.com>
2023-09-13 00:06:48 -0400

    add datepicker and dropbox

--- app/src/main/java/com/example/personas/ui/theme/persona/PersonaScreen.kt
+++ app/src/main/java/com/example/personas/ui/theme/persona/PersonaScreen.kt
@@ -1,15 +1,14 @@
 package com.example.personas.ui.theme.persona
 
-import androidx.annotation.StringRes
+import android.app.DatePickerDialog
+import android.content.Context
+import android.widget.DatePicker
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -18,29 +17,23 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.Build
 import androidx.compose.material.icons.filled.Call
 import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Email
-import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
-import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -52,7 +45,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.TextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
@@ -61,12 +53,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.dimensionResource
@@ -74,14 +63,20 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.personas.R
-import com.example.personas.data.PersonasDb
 import com.example.personas.data.local.entity.Persona
 import kotlinx.coroutines.flow.collectLatest
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import android.icu.util.Calendar
+import androidx.compose.foundation.clickable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.platform.LocalContext
+import java.util.Date
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -321,66 +316,13 @@ fun ExpandedContent(viewModel: PersonaViewModel) {
                 focusManager.moveFocus(FocusDirection.Next)
             }
         )
+        val context = LocalContext.current
+        DateTextField(viewModel = viewModel, context =context )
 
-        OutlinedTextField(
-            modifier = Modifier.fillMaxWidth(),
-            label = { Text(text = "Fecha Nacimiento") },
-            singleLine = true,
-            maxLines=1,
-            value = viewModel.fechaNac,
-            onValueChange = viewModel::onfechaNacChanged,
-            leadingIcon ={
-                Icon(imageVector = Icons.Filled.DateRange, contentDescription = "email icon")
-            },
-            isError = viewModel.fechaNacError,
-            trailingIcon = {
-                AnimatedVisibility(
-                    visible = viewModel.fechaNacError,
-                    enter = fadeIn(),
-                    exit = fadeOut()
-                ) {
-                    Icon(imageVector = Icons.Default.Warning, contentDescription = "")
-                }
-            } ,
-            keyboardOptions = KeyboardOptions(
-                keyboardType =  KeyboardType.Text,
-                imeAction = ImeAction.Next,
-                capitalization = KeyboardCapitalization.Words
-            ),
-            keyboardActions = KeyboardActions{
-                focusManager.moveFocus(FocusDirection.Next)
-            }
-        )
-        OutlinedTextField(
-            modifier = Modifier.fillMaxWidth(),
-            label = { Text(text = "Ocupacion") },
-            singleLine = true,
-            maxLines=1,
-            value = viewModel.ocupacion,
-            onValueChange = viewModel::onOcupacionChanged,
-            leadingIcon ={
-                Icon(imageVector = Icons.Filled.Build, contentDescription = "email icon")
-            },
-            isError = viewModel.ocupacionError,
-            trailingIcon = {
-                AnimatedVisibility(
-                    visible = viewModel.ocupacionError,
-                    enter = fadeIn(),
-                    exit = fadeOut()
-                ) {
-                    Icon(imageVector = Icons.Default.Warning, contentDescription = "")
-                }
-            } ,
-            keyboardOptions = KeyboardOptions(
-                keyboardType =  KeyboardType.Text,
-                imeAction = ImeAction.Next,
-                capitalization = KeyboardCapitalization.Words
-            ),
-            keyboardActions = KeyboardActions{
-                focusManager.moveFocus(FocusDirection.Down)
-                keyboardController?.hide()
-            }
-        )
+        DropdownMenuBox(viewModel)
+
+
+
         OutlinedButton(
             modifier = Modifier.fillMaxWidth(),
             onClick = {
@@ -396,6 +338,143 @@ fun ExpandedContent(viewModel: PersonaViewModel) {
 
     }
 }
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
+@Composable
+fun DateTextField(
+    viewModel: PersonaViewModel,
+    context: Context
+) {
+    val year: Int
+    val month: Int
+    val day: Int
+    val calendar = Calendar.getInstance()
+    year = calendar.get(Calendar.YEAR)
+    month = calendar.get(Calendar.MONTH)
+    day = calendar.get(Calendar.DAY_OF_MONTH)
+    calendar.time = Date()
+
+    var isDatePickerVisible by remember { mutableStateOf(false) }
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    OutlinedTextField(
+        modifier = Modifier.fillMaxWidth(),
+        label = { Text(text = "Fecha Nacimiento") },
+        singleLine = true,
+        maxLines=1,
+        value = viewModel.fechaNac,
+        onValueChange = viewModel::onfechaNacChanged,
+        leadingIcon ={
+            IconButton(
+                onClick = {
+                    keyboardController?.hide()
+                    isDatePickerVisible = true
+                }
+            ) {
+                Icon(imageVector = Icons.Filled.DateRange, contentDescription = "email icon")
+            }
+        },
+        isError = viewModel.fechaNacError,
+        trailingIcon = {
+            AnimatedVisibility(
+                visible = viewModel.fechaNacError,
+                enter = fadeIn(),
+                exit = fadeOut()
+            ) {
+                Icon(imageVector = Icons.Default.Warning, contentDescription = "")
+            }
+        },
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Text,
+            imeAction = ImeAction.Done
+        ),
+        keyboardActions = KeyboardActions(
+            onDone = {
+                keyboardController?.hide()
+            }
+        )
+
+    )
+    if (isDatePickerVisible) {
+        val datePickerDialog = DatePickerDialog(
+            context,
+            { _: DatePicker, year: Int, month: Int, dayOfMonth: Int ->
+                viewModel.fechaNac = "$dayOfMonth/$month/$year"
+                isDatePickerVisible = false
+            },
+            year, month, day
+        )
+        DisposableEffect(Unit) {
+            datePickerDialog.show()
+            onDispose {
+                datePickerDialog.dismiss()
+                isDatePickerVisible = false
+            }
+        }
+    }
+
+
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DropdownMenuBox(
+    viewModel: PersonaViewModel
+) {
+
+    val Opciones = arrayOf("ingeniero", "Contable", "Medico", "Abogado", "Civil")
+    var expanded by remember { mutableStateOf(false) }
+
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(3.dp)
+
+    ) {
+        ExposedDropdownMenuBox(
+            expanded = expanded,
+            onExpandedChange = {
+                expanded = !expanded
+            }
+        ) {
+            OutlinedTextField(
+
+                value = viewModel.ocupacion,
+                label = { Text(text = "Ocupacion") },
+                onValueChange = viewModel::onOcupacionChanged,
+                readOnly = true,
+                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+                modifier = Modifier
+                    .menuAnchor()
+                    .fillMaxWidth(),
+                leadingIcon ={
+                    Icon(imageVector = Icons.Filled.Build, contentDescription = "email icon")
+                }
+            )
+
+            ExposedDropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false },
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                Opciones.forEach { item ->
+                    DropdownMenuItem(
+                        text = { Text(text = item) },
+                        onClick = {
+                            viewModel.ocupacion = item
+                            expanded = false
+
+                        }
+                    )
+                }
+            }
+        }
+    }
+}
+
+
 
 
 @Composable
@@ -550,7 +629,7 @@ private fun ItemButton(
         modifier = modifier
     ) {
         Icon(
-            imageVector = if (expanded) Icons.Default.KeyboardArrowDown else Icons.Default.KeyboardArrowUp,
+            imageVector = if (expanded) Icons.Default.KeyboardArrowUp else Icons.Default.KeyboardArrowDown,
             contentDescription = "",
             tint = MaterialTheme.colorScheme.secondary
         )
